17eeb6e144ddecc0c2bba7576bc81b70c0a9ae15
BenKoehler <ben.koehler@web.de>
2022-10-16 10:48:27 +0200

    replace CMAKE_CURRENT_SOURCE_DIR with PROJECT_SOURCE_DIR

--- bk.cmake
+++ bk.cmake
@@ -1,5 +1,5 @@
-add_library(stbimage STATIC EXCLUDE_FROM_ALL ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/stb/stbimage_impl.cpp)
-target_include_directories(stbimage SYSTEM PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/stb)
+add_library(stbimage STATIC EXCLUDE_FROM_ALL ${PROJECT_SOURCE_DIR}/thirdparty/stb/stbimage_impl.cpp)
+target_include_directories(stbimage SYSTEM PUBLIC ${PROJECT_SOURCE_DIR}/thirdparty/stb)
 ConfigureCompileOptions(stbimage PUBLIC)
 
 function(link_stbimage target MOD)
