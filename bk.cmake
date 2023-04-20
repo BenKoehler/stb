28006e76521e404adb87ea6cd1ceb59d12cf1dc3
BenKoehler <ben.koehler@web.de>
2023-04-20 11:10:33 +0200

    add truetype

--- bk.cmake
+++ bk.cmake
@@ -5,3 +5,12 @@ ConfigureCompileOptions(stbimage PUBLIC)
 function(link_stbimage target MOD)
     target_link_libraries(${target} ${MOD} stbimage)
 endfunction()
+
+
+add_library(stbtruetype STATIC EXCLUDE_FROM_ALL ${PROJECT_SOURCE_DIR}/thirdparty/stb/stbtruetype_impl.cpp)
+target_include_directories(stbtruetype SYSTEM PUBLIC ${PROJECT_SOURCE_DIR}/thirdparty/stb)
+ConfigureCompileOptions(stbtruetype PUBLIC)
+
+function(link_stbtruetype target MOD)
+    target_link_libraries(${target} ${MOD} stbtruetype)
+endfunction()
---
+++ stbtruetype_impl.cpp
@@ -0,0 +1,6 @@
+#include "stbtruetype_impl.h"
+
+#define STB_TRUETYPE_IMPLEMENTATION
+
+#include "stb_truetype.h"
+
